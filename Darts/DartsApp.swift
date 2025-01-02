68c7cecb04cf1c8943a66f5f37107ae17dd43483
keshew <artyom.korotckov2017@yandex.ru>
2025-01-03 02:40:41 +0500

    Initial Commit

---
+++ Darts/DartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartsApp.swift
+//  Darts
+//
+//  Created by Артём Коротков on 03.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct DartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
